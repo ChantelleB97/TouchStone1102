34ad2592bc42dfa13e40a3b359d18f9426f93180
ChantelleB97 <rahansjenkins2.0@gmail.com>
2024-11-01 20:37:09 -0400

    ReUpdate

--- scripts.js
+++ scripts.js
@@ -2,5 +2,7 @@ let cartTotal = 0;
 
 function addToCart(price) {
     cartTotal += price;
-    document.querySelector('.cart-total').textContent = `$${cartTotal.toFixed(2)}`;
+    document.querySelector('.cart-total').innerText = `$${cartTotal.toFixed(2)}`;
 }
+
+// Optional: You can add functions to clear the cart or perform other actions if needed
